1abf78b3bf0bee6bec8ce411cdcbec4be7d0f7f4
Corey A. Schnedl <corey.schnedl@navitus.com>
2023-05-08 12:48:55 -0400

    Initial commit

---
+++ src/IntegrationBanjoCore/IntegrationBanjoCoreStack.cs
@@ -0,0 +1,13 @@
+using Amazon.CDK;
+using Constructs;
+
+namespace IntegrationBanjoCore
+{
+    public class IntegrationBanjoCoreStack : Stack
+    {
+        internal IntegrationBanjoCoreStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}

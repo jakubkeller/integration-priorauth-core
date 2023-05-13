46976ae6289d051b85fc6a3b3ee4dc699ba3323f
Corey A. Schnedl <corey.schnedl@navitus.com>
2023-05-13 11:46:53 -0400

    cicd updates

--- infra/src/Base/Config/Config.cs
+++ infra/src/Base/Config/Config.cs
@@ -11,87 +11,86 @@ namespace Integration.Banjo.Core.Config;
 
 public class CommonConfig
 {
-	public List<Environment> Environments { get; set; } = new List<Environment>();
-	public Environment GetSandboxEnvironment()
-	{
-		return Environments.First(e => e.Enviornment == EnviornmentType.Sandbox);
-	}
+    public List<Environment> Environments { get; set; } = new List<Environment>();
+    public Environment GetSandboxEnvironment()
+    {
+        return Environments.First(e => e.Enviornment == EnviornmentType.Sandbox);
+    }
 }
 public class Environment
 {
-	public string Name { get; set; } = default!;
-	public string DisplayName { get; set; } = default!;
-	public string AccountId { get; set; } = default!;
-	public string VpcId { get; set; } = default!;
-	public EnviornmentType Enviornment
-	{
-		get
-		{
-			// map string to enum
-			if (Name == "sandbox")
-			{
-				return EnviornmentType.Sandbox;
-			}
-			else if (Name == "dev")
-			{
-				return EnviornmentType.Dev;
-			}
-			else if (Name == "test")
-			{
-				return EnviornmentType.Test;
-			}
-			else if (Name == "stage")
-			{
-				return EnviornmentType.Stage;
-			}
-			else if (Name == "prod")
-			{
-				return EnviornmentType.Prod;
-			}
-			else
-			{
-				// throw exception if the environment is not supported
-				throw new Exception($"Environment {Name} is not supported");
-			}
-		}
-	}
+    public string Name { get; set; } = default!;
+    public string DisplayName { get; set; } = default!;
+    public string AccountId { get; set; } = default!;
+    public string VpcId { get; set; } = default!;
+    public EnviornmentType Enviornment
+    {
+        get
+        {
+            // map string to enum
+            if (Name == "sandbox")
+            {
+                return EnviornmentType.Sandbox;
+            }
+            else if (Name == "dev")
+            {
+                return EnviornmentType.Dev;
+            }
+            else if (Name == "test")
+            {
+                return EnviornmentType.Test;
+            }
+            else if (Name == "stage")
+            {
+                return EnviornmentType.Stage;
+            }
+            else if (Name == "prod")
+            {
+                return EnviornmentType.Prod;
+            }
+            else
+            {
+                // throw exception if the environment is not supported
+                throw new Exception($"Environment {Name} is not supported");
+            }
+        }
+    }
 }
 
 public enum EnviornmentType
 {
-	Sandbox = 0,
-	Dev = 1,
-	Test = 2,
-	Stage = 3,
-	Prod = 4,
+    Sandbox = 0,
+    Dev = 1,
+    Test = 2,
+    Stage = 3,
+    Prod = 4,
 }
 
 public static class ConfigHelper
 {
-	private static Environment _currentEnvironment = null;
-	public static Environment GetCurrentEnvironment(string accountId=null)
-	{
-		if (_currentEnvironment != null)
-		{
-			return _currentEnvironment;
-		}
-		// read config json file from assembly location where nuget package is installed
+    private static Environment _currentEnvironment = null;
+    public static Environment GetCurrentEnvironment(string accountId = null)
+    {
+        if (_currentEnvironment != null)
+        {
+            return _currentEnvironment;
+        }
+        // read config json file from assembly location where nuget package is installed
         string assemblyDirectoryPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!;
-		Console.WriteLine($"Assembly Directory Path: {assemblyDirectoryPath}");
         string configFilePath = Path.Combine(assemblyDirectoryPath, "config.json");
 
-		var config = JsonSerializer.Deserialize<CommonConfig>(File.ReadAllText(configFilePath))!;
-		if (string.IsNullOrEmpty(accountId))
-		{
-			// if enviorment variable is not set, use Sandbox as default
-			accountId = config.GetSandboxEnvironment().AccountId;
-		}
-		Console.WriteLine($"AWS Account id: {accountId}");
+        var config = JsonSerializer.Deserialize<CommonConfig>(File.ReadAllText(configFilePath))!;
+        if (string.IsNullOrEmpty(accountId))
+        {
+            // if enviorment variable is not set, use Sandbox as default
+            accountId = config.GetSandboxEnvironment().AccountId;
+        }
+        Console.WriteLine($"AWS Account id: {accountId}");
 
-		// read config json file into dictionary
-		_currentEnvironment = config.Environments.First(e => e.AccountId == accountId);
+        // read config json file into dictionary
+        _currentEnvironment = config.Environments.FirstOrDefault(e => e.AccountId == accountId, config.GetSandboxEnvironment());
 
-		Console.WriteLine($"Current Environment: {_currentEnvironment.DisplayName}");
-		return _currentEnvironment;
-	}
+        Console.WriteLine($"Current Environment: {_currentEnvironment.DisplayName}");
+        return _currentEnvironment;
+    }
 }
--- scripts/cicd/dotnet/pack.sh
+++ scripts/cicd/dotnet/pack.sh
@@ -9,5 +9,5 @@ pushd infra/src/Base
 PATCH_NUMBER=${BUILD_BUILDID:-0}
 # get ado staging path
 OUTPUT_PATH=${BUILD_ARTIFACTSTAGINGDIRECTORY:-$ROOT_DIR/dist}
-dotnet pack -c Release -o $OUTPUT_PATH/Integration.Banjo.Base.Package -p:PackageVersion=1.1."$PATCH_NUMBER"
+dotnet pack -c Release -o $OUTPUT_PATH/Integration.Banjo.Base.Package -p:PackageVersion=1.1."$PATCH_NUMBER" -p:PackageID="Integration.Banjo.Base"
 popd
